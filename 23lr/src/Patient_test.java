53ce6c5ac91bf800a0ab24c8948b2d3f63c98149
adm <emets@knu.ua>
2018-12-04 00:43:34 +0200

    2.4 - 50/50, 2.3 - test

---
+++ 23lr/src/Patient_test.java
@@ -0,0 +1,102 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+class Patient_test {
+    private final long ID;
+    private final StringBuilder firstName;
+    private final StringBuilder lastName;
+    private final StringBuilder midName;
+    private final StringBuilder address;//
+    private final long phoneNum;
+    private final long numCard;
+    private final StringBuilder diagnose;
+
+    Patient_test(long ID, StringBuilder firstName,StringBuilder lastName, StringBuilder midName, StringBuilder address,
+                 long phoneNum, long numCard, StringBuilder diagnose){
+        this.ID = ID; this.firstName=firstName;this.lastName=lastName;
+        this.midName=midName; this.address=address;
+        this.phoneNum=phoneNum; this.numCard=numCard;
+        this.diagnose=diagnose;
+    }
+
+    long getID(){return ID;}
+    StringBuilder getFirstName(){return firstName;}
+    StringBuilder getLastName(){return lastName;}
+    StringBuilder getMidName(){return  midName;}
+    StringBuilder getAddress(){return address;}
+    long getPhoneNum(){return phoneNum;}
+    long getNumCard(){return numCard;}
+    StringBuilder getDiagnose(){return diagnose;}
+
+    @Override
+    public String toString() {
+        return "Ідентифікаційний номер\t" + ID + "\n" +
+                "Ім'я\t" + firstName.toString() + "\n" +
+                "По-батькові\t" + midName.toString() + "\n" +
+                "Прізвище\t" + lastName.toString() + "\n" +
+                "Адреса\t" + address.toString() + "\n" +
+                "Номер телефону\t" + phoneNum + "\n" +
+                "Номер картки\t" + numCard + "\n" +
+                "Діагноз\t" + diagnose.toString() + "\n";
+    }
+
+    public static class Builder{
+
+        Properties properties = new Properties();
+        {
+            try {
+                properties.load(new FileInputStream(new File("main.properties")));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private long ID;
+        private StringBuilder firstName = new StringBuilder(properties.getProperty("none.str")); //new StringBuilder("first name");
+        private StringBuilder lastName = new StringBuilder(properties.getProperty("none.str"));
+        private StringBuilder midName = new StringBuilder(properties.getProperty("none.str"));
+        private StringBuilder address = new StringBuilder(properties.getProperty("none.str"));
+        private long phoneNum = Long.parseLong(properties.getProperty("none.num"));
+        private long numCard = Long.parseLong(properties.getProperty("none.num"));
+        private StringBuilder diagnose = new StringBuilder(properties.getProperty("none.str"));
+
+
+        Builder setID(long ID){
+            this.ID = ID;
+            return this;
+        }
+        Builder setFirstName(StringBuilder firstName){
+            this.firstName = firstName;
+            return this;
+        }
+        Builder setLastName(StringBuilder lastName){
+            this.lastName = lastName;
+            return this;
+        }
+        Builder setMidName(StringBuilder midName){
+            this.midName = midName;
+            return this;
+        }
+        Builder setAddress(StringBuilder address){
+            this.address = address;
+            return this;
+        }
+        Builder setPhoneNum(long phoneNum){
+            this.phoneNum = phoneNum;
+            return this;
+        }
+        Builder setNumCard(long numCard){
+            this.numCard = numCard;
+            return this;
+        }
+        Builder setDiagnose(StringBuilder diagnose){
+            this.diagnose = diagnose;
+            return this;
+        }
+        public Patient_test build(){
+            return new Patient_test(ID, firstName, lastName, midName, address, phoneNum, numCard, diagnose);
+        }
+    }
+}
---
+++ 23lr/src/main.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+public class main {
+    static Scanner in = new Scanner(System.in);
+    static long ID;
+    static StringBuilder firstName;
+    static StringBuilder lastName;
+    static StringBuilder midName;
+    static StringBuilder address;//
+    static long phoneNum;
+    static long numCard;
+    static StringBuilder diagnose;
+
+    private static void inputData(){
+        var out = System.out;
+        out.print("Enter id\t");
+        ID = Long.parseLong(in.next());
+
+        out.print("Enter first name\t");
+        firstName = new StringBuilder(in.nextLine());
+
+        out.print("Enter middle name\t");
+        midName = new StringBuilder(in.nextLine());
+
+        out.print("Enter last name\t");
+        lastName = new StringBuilder(in.nextLine());
+
+        out.print("Enter address\t");
+        address = new StringBuilder(in.nextLine());
+        address.append(in.nextLine());
+
+        out.print("Enter phone number\t");
+        phoneNum = Long.parseLong(in.next());
+
+        out.print("Enter number of card\t");
+        numCard = Long.parseLong(in.next());
+
+        out.print("Enter diagnose\t");
+        diagnose = new StringBuilder(in.nextLine());
+        diagnose.append(in.nextLine());
+    }
+    private static void checkNullData(){
+        if(ID<=0 || phoneNum<=0 || numCard<=0) {throw new NumberFormatException();}
+        if(firstName==null || lastName==null || midName==null || address==null || diagnose==null){
+            throw new IllegalArgumentException();}
+    }
+    public static void main(String[] args){
+        var out = System.out;
+        try{
+            inputData();
+            checkNullData();
+        }catch(NumberFormatException num){
+            out.println(num.getMessage());
+            out.println(num.getCause());
+            out.print("Fill data one more time? y/n\t");
+            if(in.next().charAt(0)=='y') {
+                inputData();
+                checkNullData();
+            }
+            else System.exit(0);
+        }catch (IllegalArgumentException ex){
+            out.println(ex.getMessage());
+            out.println(ex.getCause());
+            out.print("Fill data one more time? y/n\t");
+            if(in.next().charAt(0)=='y') {
+                inputData();
+                checkNullData();
+            }
+            else System.exit(0);
+        }catch (Exception ex){
+            out.println(ex.getMessage());
+            out.println(ex.getCause());
+            out.print("Fill data one more time? y/n\t");
+            if(in.next().charAt(0)=='y') {
+                inputData();
+                checkNullData();
+            }
+            else System.exit(0);
+        }
+        var p1 = new Patient_test(ID, firstName, lastName, midName, address,phoneNum,numCard,diagnose);
+        out.print(p1.toString());
+    }
+}
